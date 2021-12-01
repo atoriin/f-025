279802e5e6867b12f3ec879ad96d36d404ab3b25
atorii <nyacochin@gmail.com>
2021-12-01 16:18:49 +0900

    deploy test

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,6 +2,6 @@
 # --dry-run
 
 #ポートフォリオ
-rsync --delete -auvz -e "ssh -p 10022 -i ~/.ssh/id_xserver_rsa" --exclude-from=./script/exclude.txt -rlpt ./ nyacochin@sv6068.xserver.jp:f-025.com/public_html/
+#rsync --delete -auvz -e "ssh -p 10022 -i ~/.ssh/id_xserver_rsa" --exclude-from=./script/exclude.txt -rlpt ./ nyacochin@sv6068.xserver.jp:f-025.com/public_html/
 
 
